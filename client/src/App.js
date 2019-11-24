c2e346f3d58ce67f686d8ade2972248a81d67704
Michael Giebner <mikegiebner@gmail.com>
2019-11-24 18:26:41 -0500

    Changed functional components to class components. Part of the way through C6-6-34

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
@@ -6,21 +6,23 @@ import Register from "./components/auth/Register"
 import Login from "./components/auth/Login"
 import './App.css';
 
-const App = () => {
-  return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={ Landing } />
-        <section className="container">
-          <Switch>
-            <Route exact path="/register" component={ Register } />
-            <Route exact path="/login" component={ Login } />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Login = () => {
-   return (
-      <div>
-         Login
-      </div>
-   )
+class Login extends Component {
+   render() {
+      return (
+         <div>
+            Login
+         </div>
+      )
+   }
 }
 
 export default Login
